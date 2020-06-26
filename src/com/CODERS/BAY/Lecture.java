8485b0f764847eee6a7d61803e0eddd36c09c808
Simon <s.lechner.biz@gmail.com>
2020-06-26 11:35:42 +0200

    Implemented Student, Lecture and Assignments, butchering main and implementing tests

---
+++ src/com/CODERS/BAY/Lecture.java
@@ -0,0 +1,63 @@
+package com.CODERS.BAY;
+
+import java.util.ArrayList;
+
+public class Lecture {
+    private String name;
+    private ArrayList<Assignment> assignedAssignments = new ArrayList<>();
+
+    //Contructors
+    public Lecture(String name) {
+        this.name = name;
+    }
+
+    public Lecture(String name, ArrayList<Assignment> assignedAssignments) {
+        this.name = name;
+        this.assignedAssignments = assignedAssignments;
+    }
+
+    //Methods
+    public Integer getGrade(){
+        Double gradeTotal = 0.00;
+        Double gradeCount = 0.00;
+        for(Assignment a : assignedAssignments){
+            if(a.isGraded()){
+                gradeTotal += a.getGrade();
+                gradeCount++;
+            } else {
+                System.out.println(a.getName()+" is yet to be graded!");
+            }
+        }
+        if(gradeCount == 0.00){
+            return null;
+        } else {
+            return (Math.toIntExact(Math.round(gradeTotal / gradeCount)));
+        }
+    }
+
+
+    //Getter & Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Assignment> getAssignedAssignments() {
+        return assignedAssignments;
+    }
+
+    public void setAssignedAssignments(ArrayList<Assignment> assignedAssignments) {
+        this.assignedAssignments = assignedAssignments;
+    }
+
+    @Override
+    public String toString() {
+        return "Lecture{" +
+                "name='" + name + '\'' +
+                ", assignedAssignments=" + assignedAssignments +
+                '}';
+    }
+}
---
+++ test/src/LectureTest.java
@@ -0,0 +1,54 @@
+import com.CODERS.BAY.Assignment;
+import com.CODERS.BAY.Lecture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LectureTest {
+
+    @Test
+    @DisplayName("A Lecture without any assignments returns null for the grade")
+    public void testLectureWithoutAssignments() {
+        // TODO implement
+        Lecture testLecture = new Lecture("test", new ArrayList<Assignment>());
+        assertEquals(null, testLecture.getGrade());
+    }
+
+    @Test
+    @DisplayName("A Lecture with exactly one assignments returns the grade of that assignment")
+    public void testLectureWithSingleAssignment() {
+        // TODO implement
+        Assignment testAssignment = new Assignment("testA", 100d);
+        testAssignment.grade(100d);
+        ArrayList<Assignment> testArray = new ArrayList<>();
+        testArray.add(testAssignment);
+        Lecture testLecture = new Lecture("testL", testArray);
+        assertEquals(1, testLecture.getGrade());
+    }
+
+    @Test
+    @DisplayName("A Lecture with many assignments returns the average grade of these assignments")
+    public void testLectureWithMultipleAssignment() {
+        // TODO implement
+        Assignment testAssignment1 = new Assignment("testA1",100d);
+        Assignment testAssignment2 = new Assignment("testA2",100d);
+        Assignment testAssignment3 = new Assignment("testA3",100d);
+
+        testAssignment1.grade(100d);
+        testAssignment2.grade(75d);
+        testAssignment3.grade(49d);
+
+        ArrayList<Assignment> testArray = new ArrayList<>();
+        testArray.add(testAssignment1);
+        testArray.add(testAssignment2);
+        testArray.add(testAssignment3);
+
+        Lecture testLecture = new Lecture("testL", testArray);
+
+        assertEquals(3, testLecture.getGrade());
+    }
+
+}
