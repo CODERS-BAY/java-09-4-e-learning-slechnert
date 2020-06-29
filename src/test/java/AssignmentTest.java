4815d5db24c638cdaab3169b3d8808372871762b
Simon <s.lechner.biz@gmail.com>
2020-06-29 10:18:29 +0200

    cleanup

--- src/test/java/AssignmentTest.java
+++ src/test/java/AssignmentTest.java
@@ -1,3 +1,4 @@
+import com.CODERS.BAY.Assignment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,22 +9,22 @@ public class AssignmentTest {
     @Test
     @DisplayName("An assignment that is graded with less than 50 percent of the points results in a Grade 5")
     public void testGradeBadAssignment() {
-        // TODO
-        assertEquals(true, true);
+        Assignment testAssignment = new Assignment("test1", 100d);
+        assertEquals(5, testAssignment.grade(40d));
     }
 
     @Test
     @DisplayName("An assignment that is graded with 100 percent of the points results in a Grade 1")
     public void testGradeGoodAssignment() {
-        // TODO
-        assertEquals(true, true);
+        Assignment testAssignment = new Assignment("test2", 100d);
+        assertEquals(1, testAssignment.grade(100d));
     }
 
     @Test
     @DisplayName("An assignment that is graded with more than 100 percent of the points results in a Grade 1")
     public void testGradeGreatAssignment() {
-        // TODO
-        assertEquals(true, true);
+        Assignment testAssignment = new Assignment("test3", 100d);
+        assertEquals(1, testAssignment.grade(111d));
     }
 
 }
